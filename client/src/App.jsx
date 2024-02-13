f828e9738d3c4cb6806ab5541e4c6100cbc08be6
kveszter <virag.eszt@gmail.com>
2024-02-13 12:01:48 +0100

    mainPage_renders_and_navigates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect, createContext } from 'react';
 import "./App.css";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Register from './Pages/MainPage/Register/Register.jsx';
+import Register from './Pages/Register/Register.jsx';
 import Login from './Pages/Login/Login.jsx';
 import UserProfile from './Components/UserProfile/UserProfile.jsx';
 import MainPage from './Pages/MainPage/MainPage.jsx';
+import UnderConstruction from './Pages/UnderConstruction/UnderConstruction.jsx';
+import PlayMode from './Pages/PlayMode/PlayMode.jsx';
 
 import { Layout } from './Pages/Layout/Layout.jsx'
 
@@ -14,8 +16,6 @@ export const UserNameContext = createContext("")
 function App() {
   const [validUser, setValidUser] = useState(false); //🔰 needs to be false once done
   const [userName, setUserName] = useState("");
-
-
 
   return (
     <UserNameContext.Provider value={{ userName, setUserName }}>
@@ -27,6 +27,8 @@ function App() {
               <Route path='/login' element={<Login />}></Route>
               <Route path='/' element={<MainPage />}></Route>
               <Route path='/profile' element={<UserProfile />}></Route>
+              <Route path='/underconstruction' element={<UnderConstruction />}></Route>
+              <Route path='/play' element={<PlayMode />}></Route>
             </Route>
           </Routes>
         </BrowserRouter >
---
+++ client/src/Pages/PlayMode/PlayMode.jsx
@@ -0,0 +1,3 @@
+export default function PlayMode() {
+  return <p>Hi, this is play mode</p>
+}
