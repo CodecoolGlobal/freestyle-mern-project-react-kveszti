1897c3906a9f43d60d8a52a24ff6943026264197
kveszter <virag.eszt@gmail.com>
2024-02-14 21:40:37 +0100

    css_reactions_for_answers_done

--- client/src/Components/QuestionsAndAnswers/QuestionsAndAnswers.jsx
+++ client/src/Components/QuestionsAndAnswers/QuestionsAndAnswers.jsx
@@ -6,7 +6,8 @@ export default function QuestionsAndAnswers({ questionsArray }) {
   const [allAnswersArray, setAllAnswersArray] = useState([]);
   const [objectifiedArrayIncorrect, setObjectifiedArrayIncorrect] = useState([])
   const [correctAnswerObject, setCorrectAnswerObject] = useState({})
-  const [allAnswers, setAllAnswers] = useState([])
+  const [allAnswers, setAllAnswers] = useState([]);
+
   const navigate = useNavigate();
   const abc = ["A", "B", "C", "D"];
   const seconds = 1;
@@ -55,27 +56,47 @@ export default function QuestionsAndAnswers({ questionsArray }) {
     return array;
   }
 
-  function handleAnswerSelect(isCorrect) {
-    if (questionIndex < questionsArray.length - 1) {
-      //Eszti
-      //
-      //Zsani
-      //
-      setQuestionIndex((previndex) => previndex + 1);
+  function handleAnswerSelect(isCorrect, eventTarget) {
+    //Eszti
+    //
+    while (eventTarget && !eventTarget.classList.contains("answerCont")) {
+      eventTarget = eventTarget.parentElement;
+    }
+    const answerDiv = document.getElementById(eventTarget.id);
+    answerDiv.classList.add(isCorrect ? "correct-answer-blink" : "wrong-answer");
+
+    if (isCorrect) {
+      setTimeout(() => {
+        answerDiv.classList.remove("correct-answer-blink");
+        if (questionIndex < questionsArray.length - 1) {
+          setQuestionIndex(prevIndex => prevIndex + 1);
+        } else {
+          navigate("/");
+        }
+      }, 2000);
     } else {
-      //Eszti
-      //
-      //Zsani
-      //
-      navigate("/");
+      const correctAnswerIndex = allAnswersArray.findIndex(answer => answer.isCorrect === true);
+      console.log(correctAnswerIndex)
+      const correctAnswerDiv = document.getElementById(`answer${correctAnswerIndex}`);
+      console.log(correctAnswerDiv)
+      correctAnswerDiv.classList.add("correct-answer");
+
+      setTimeout(() => {
+        answerDiv.classList.remove("wrong-answer");
+        correctAnswerDiv.classList.remove("correct-answer");
+        if (questionIndex < questionsArray.length - 1) {
+          setQuestionIndex(prevIndex => prevIndex + 1);
+        } else {
+          navigate("/");
+        }
+      }, 2000);
     }
   }
 
-
   return (<div className="QAndACont">
     <div id="question" className="questionCont">{he.decode(questionsArray[questionIndex].question)}</div>
-    <div className="answersCont">{allAnswers.map((obj, index) => {
-      return <><div className="answerCont" onClick={() => setTimeout(() => handleAnswerSelect(obj.isCorrect), 1000 * seconds)}>
+    <div className="answersCont">{allAnswersArray.map((obj, index) => {
+      return <><div id={"answer" + index} className="answerCont" onClick={(event) => handleAnswerSelect(obj.isCorrect, event.target)}>
         <div className="answerIndex"><div className="answerIndexText">{abc[index]}</div></div>
         <div className="answerText">{obj.text}</div>
       </div></>
